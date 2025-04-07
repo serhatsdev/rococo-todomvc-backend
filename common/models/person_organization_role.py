203e5e6f5c7f8ddb1e7f5f0429387f0fb678f6cd
Serhat S <68604417+serhatsdev@users.noreply.github.com>
2025-04-07 20:52:49 +0300

    add `PersonOrganizationRoleEnum`

--- common/models/person_organization_role.py
+++ common/models/person_organization_role.py
@@ -1,5 +1,10 @@
+from enum import Enum
 from rococo.models import PersonOrganizationRole as BasePersonOrganizationRole
 
+class PersonOrganizationRoleEnum(Enum):
+    OWNER = "OWNER"
+    MANAGER = "MANAGER"
+    MEMBER = "MEMBER"
 
 class PersonOrganizationRole(BasePersonOrganizationRole):
     pass
